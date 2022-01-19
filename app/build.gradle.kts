d7303f8e74a53fd457173bb0f3e9d555209f5520
takusan23 <takumu1200@gmail.com>
2022-01-20 00:03:36 +0900

    説明を追加した。 1.2.1を出す

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "io.github.takusan23.mobilestatuswidget"
         minSdk = 24
         targetSdk = 31
-        versionCode = 3
-        versionName = "1.2.0"
+        versionCode = 4
+        versionName = "1.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
