fa4738fb0bb311cbc24b6d4015d3e2411b63a160
takusan23 <takumu1200@gmail.com>
2024-07-27 01:52:09 +0900

    1.3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "io.github.takusan23.mobilestatuswidget"
         minSdk = 24
         targetSdk = 34
-        versionCode = 5
-        versionName = "1.2.2"
+        versionCode = 6
+        versionName = "1.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
